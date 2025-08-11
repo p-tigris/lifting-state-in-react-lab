fad9332daf42b99902afaba2dcbcb728b5cb7fb2
smdhar <shoumik_dhar@yahoo.com>
2025-08-11 01:43:00 -0400

    Lab completed

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,12 @@
+const BurgerStack = (props) => {
+  return <ul>
+    {props.ingredients.map((ingredient, index) => {
+    return <li key={index} style={{ backgroundColor: ingredient.color }}>
+        {ingredient.name}
+        <button onClick={() => props.removeFromBurger(index)}>X</button>
+    </li>
+    })}
+  </ul>;
+};
+
+export default BurgerStack;
